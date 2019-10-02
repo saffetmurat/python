135d3ea216d6992d8b6166b5b6c43704ed32ee52
saffetmurat <53378755+saffetmurat@users.noreply.github.com>
2019-10-02 13:08:38 +0300

    ikinci commit
    Burada time modülü kullanılarak programın hemen kapanması engelleniyor. Böylece kullanıcıya ekranı incelemesi için yeterli zaman verilmiş olunuyor. Ayrıca tamsayı girilmediğinde except kısmında oluşan sıkıntı önlendi.

--- BasamakSayisiniBulma.py
+++ BasamakSayisiniBulma.py
@@ -1,8 +1,16 @@
+import time
+
 baslik="Basamak sayısını bulan program"
 print("-"*len(baslik),baslik,"-"*len(baslik),sep="\n",end="\n\n")
 
 try:
-    sayi = int(input("Bir tam sayı giriniz :"))
+    sayi = input("Bir tam sayı giriniz :")
+    #sayi = int(input("Bir tam sayı giriniz :")) şeklinde olursa 
+    #except kısmında sayi değişkeni tanımlanmadı deyip hata veriyor.
+    #Hata alınmadan önce sayi değişkeni oluşturulyor.
+    #Ardından dönüşüm yapılıyor. Dönüşümde hata olunca bu sefer sayi değişkeni önceden tanımlandığı
+    #için except kısmında sıkıntı çıkmıyor.
+    sayi = int(sayi)
     basamaklar=[]
 
     for s in str(sayi):
@@ -10,9 +18,16 @@ try:
 
     if sayi < 0:
         print(sayi, "negatif sayısının sahip olduğu basamaklardaki rakamlar =>", basamaklar[1:],"\nToplam basamak sayısı",len(basamaklar)-1)
+
     else:
         print(sayi, "pozitif sayısının sahip olduğu basamaklardaki rakamlar =>", basamaklar,"\nToplam basamak sayısı",len(basamaklar))
 
-
 except:
-    print("{} bir tam sayı değil!".format(sayi))
+    print("\"{}\" bir tam sayı değil!".format(sayi))
+
+finally:
+    sayac = 20
+    while sayac>0:
+        print("\rProgram Kapatılıyor. Son {} sn..".format(sayac),end="")
+        time.sleep(1)
+        sayac -= 1
